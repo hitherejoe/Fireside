49af8d376efb4ec9a419b11bbbf1da34895c44b5
Joe Birch <hitherejoebirch@googlemail.com>
2017-08-22 16:51:06 +0100

    Commit content for setting defaults step

---
+++ app/src/main/java/co/joebirch/fireside/FaqActivity.java
@@ -0,0 +1,17 @@
+package co.joebirch.fireside;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FaqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("FAQ");
+    }
+
+}
--- app/src/main/java/co/joebirch/fireside/MainActivity.java
+++ app/src/main/java/co/joebirch/fireside/MainActivity.java
@@ -1,13 +1,19 @@
 package co.joebirch.fireside;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PARAMETER_SHOULD_SHOW_CHAT = "should_show_chat";
+
     private FirebaseRemoteConfig firebaseRemoteConfig;
+    private boolean shouldShowChatActivity = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
+        firebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
+
+        FirebaseRemoteConfigSettings configSettings =
+                new FirebaseRemoteConfigSettings.Builder()
+                        .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                        .build();
+        firebaseRemoteConfig.setConfigSettings(configSettings);
+        shouldShowChatActivity = firebaseRemoteConfig.getBoolean(PARAMETER_SHOULD_SHOW_CHAT);
+
+        setupHelpButton();
+    }
+
+    private void setupHelpButton() {
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (shouldShowChatActivity) {
+                    startChatActivity();
+                } else {
+                    startFaqActivity();
+                }
+            }
+        });
+    }
+
+    private void startChatActivity() {
+        startActivity(new Intent(this, ChatActivity.class));
+    }
+
+    private void startFaqActivity() {
+        startActivity(new Intent(this, FaqActivity.class));
     }
 
 }
