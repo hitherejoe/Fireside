032440c08091cdb4b355d6b9b86cf20067175f74
Joe Birch <hitherejoebirch@googlemail.com>
2017-08-22 16:41:59 +0100

    Commit code for Setup step

---
+++ app/src/main/java/co/joebirch/fireside/MainActivity.java
@@ -0,0 +1,20 @@
+package co.joebirch.fireside;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseRemoteConfig firebaseRemoteConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+    }
+
+}
